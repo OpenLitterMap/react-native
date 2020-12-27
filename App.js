ac7a9fd74703dd92dd4af878439c1eeda10d36ee
xlcrr <info@openlittermap.com>
2020-12-27 15:26:12 +0000

    code cleanup on albums/media list

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
-import { Text } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import configureStore from './store'
@@ -8,8 +7,12 @@ import configureStore from './store'
 // import Icon from 'react-native-vector-icons/MaterialIcons';
 // Icon.loadFont();
 
-import './i18n'
 import RootContainer from './screens/RootContainer'
+
+// the hoc
+// import i18n from './i18n'
+// import { I18nextProvider } from 'react-i18next';
+// import { withTranslation } from 'react-i18next';
 
 const App: () => React$Node = () => {
 
--- screens/pages/components/albums/src/components/AlbumsList/index.js
+++ screens/pages/components/albums/src/components/AlbumsList/index.js
@@ -5,29 +5,30 @@ import AlbumItem from '../AlbumItem';
 import styles from './styles';
 
 class AlbumsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
-  render() {
-    let { albums } = this.props;
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-    return (
-      <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
-        {albums.map((album, index) => (
-          <AlbumItem
-            key={index}
-            albumName={album.albumName}
-            thumbnail={album.images && album.images[0].image.uri}
-            counter={album.images ? album.images.length : 0}
-            index={index}
-            onAlbumPress={this.props.onAlbumPress}
-          />
-        ))}
-      </ScrollView>
-    );
-  }
+    render() {
+        let { albums } = this.props;
+
+        return (
+            <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
+                {albums.map((album, index) => (
+                    <AlbumItem
+                        key={index}
+                        albumName={album.albumName}
+                        thumbnail={album.images && album.images[0].image.uri}
+                        counter={album.images ? album.images.length : 0}
+                        index={index}
+                        onAlbumPress={this.props.onAlbumPress}
+                    />
+                ))}
+            </ScrollView>
+        );
+    }
 }
 
 export default AlbumsList;
