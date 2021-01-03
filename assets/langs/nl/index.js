f44a315151de41f54b892e9551164d6b5a53ae18
Steenbergen <suzefred@gmail.com>
2021-01-03 15:50:25 +0100

    created folder nl and translated the json-files

---
+++ assets/langs/nl/index.js
@@ -0,0 +1,15 @@
+import auth from './auth.json'
+import leftpage from './leftpage.json'
+import litter from './litter.json'
+import settings from './settings.json'
+import tag from './tag.json'
+import welcome from './welcome.json'
+
+export const en = {
+    auth,
+    leftpage,
+    litter,
+    settings,
+    tag,
+    welcome
+}
