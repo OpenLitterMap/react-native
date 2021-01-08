221863aed70841adf5b6eacc28f40374c94ea3c5
xlcrr <info@openlittermap.com>
2021-01-08 21:01:11 +0000

    portugese

--- assets/langs/index.js
+++ assets/langs/index.js
@@ -2,10 +2,12 @@ import { de } from './de'
 import { en } from './en'
 import { es } from './es'
 import { nl } from './nl'
+import { pt } from  './pt'
 
 export const langs = {
     de,
     en,
     es,
-    nl
+    nl,
+    pt
 };
