14d687e644c5862f47350b4aa3aae38287828429
deznit <deznit.official@gmail.com>
2021-07-23 16:47:02 +0530

    chores: upgrading rn-vector-icons to include new icons
    config icons of tabs

--- routes/tabRoutes.js
+++ routes/tabRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-elements';
+// import { Icon } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { HomeScreen } from '../screens';
 
 const Tab = createBottomTabNavigator();
@@ -8,34 +9,38 @@ const Tab = createBottomTabNavigator();
 const TabRoutes = () => (
     <Tab.Navigator
         initialRouteName="HOME"
-        // screenOptions={({ route }) => ({
-        //     tabBarIcon: ({ focused, color, size }) => {
-        //         let iconName;
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-        //         if (route.name === 'HOME') {
-        //             iconName = 'home';
-        //         } else if (route.name === 'SEARCH') {
-        //             iconName = 'insights';
-        //         } else if (route.name === 'NEW') {
-        //             iconName = 'people-outline';
-        //         } else if (route.name === 'NOTIFICATION') {
-        //             iconName = 'trophy-outline';
-        //         } else if (route.name === 'PROFILE') {
-        //             iconName = 'cloud-upload-outline';
-        //         }
+                if (route.name === 'HOME') {
+                    iconName = focused ? 'ios-home' : 'ios-home-outline';
+                } else if (route.name === 'STATS') {
+                    iconName = focused
+                        ? 'ios-trending-up'
+                        : 'ios-trending-up-outline';
+                } else if (route.name === 'TEAM') {
+                    iconName = focused ? 'ios-people' : 'ios-people-outline';
+                } else if (route.name === 'RANKING') {
+                    iconName = focused ? 'ios-trophy' : 'ios-trophy-outline';
+                } else if (route.name === 'UPLOAD') {
+                    iconName = focused
+                        ? 'ios-cloud-upload'
+                        : 'ios-cloud-upload-outline';
+                }
 
-        //         // You can return any component that you like here!
+                // You can return any component that you like here!
 
-        //         return (
-        //             // @ts-ignore
-        //             <Icon name={iconName} size={24} color={color} />
-        //         );
-        //     }
-        // })}
+                return (
+                    // @ts-ignore
+                    <Icon name={iconName} size={24} color={color} />
+                );
+            }
+        })}
         tabBarOptions={{
             activeTintColor: 'black',
             inactiveTintColor: 'gray',
-            // showLabel: false,
+            showLabel: false,
             style: {
                 backgroundColor: 'white',
                 borderTopWidth: 0,
@@ -44,9 +49,10 @@ const TabRoutes = () => (
             }
         }}>
         <Tab.Screen name="HOME" component={HomeScreen} />
-        <Tab.Screen name="SEARCH" component={HomeScreen} />
-        <Tab.Screen name="NOTIFICATION" component={HomeScreen} />
-        <Tab.Screen name="PROFILE" component={HomeScreen} />
+        <Tab.Screen name="STATS" component={HomeScreen} />
+        <Tab.Screen name="TEAM" component={HomeScreen} />
+        <Tab.Screen name="RANKING" component={HomeScreen} />
+        <Tab.Screen name="UPLOAD" component={HomeScreen} />
     </Tab.Navigator>
 );
 
