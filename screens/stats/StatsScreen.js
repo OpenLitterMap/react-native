4e1885879f29e3812bd65fc9971687e43670f846
deznit <deznit.official@gmail.com>
2021-07-30 13:43:31 +0530

    feat: stats screen dummy

--- screens/stats/StatsScreen.js
+++ screens/stats/StatsScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Header, Title, Colors } from '../components';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+import { Header, Title, Colors, AnimatedCircle } from '../components';
+import { IconStatsCard } from './_components';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 class StatsScreen extends Component {
@@ -8,18 +9,77 @@ class StatsScreen extends Component {
         return (
             <>
                 <Header
-                    leftContent={<Title>Stats</Title>}
+                    leftContent={<Title color="white">Stats</Title>}
                     rightContent={
                         <Icon
                             name="ios-share-outline"
                             size={24}
-                            color={Colors.text}
+                            color={Colors.white}
                         />
                     }
                 />
-                <View style={styles.container}>
-                    <Text> Stats screen </Text>
-                </View>
+                <ScrollView
+                    contentContainerStyle={{
+                        paddingBottom: 100,
+                        paddingTop: 20
+                    }}
+                    style={styles.container}
+                    showsVerticalScrollIndicator={false}
+                    alwaysBounceVertical={false}>
+                    <AnimatedCircle
+                        strokeWidth={30}
+                        percentage={50}
+                        color={`${Colors.accent}`}
+                        value={29.3}
+                        delay={500}
+                        radius={150}
+                    />
+
+                    <View style={styles.statsContainer}>
+                        <View style={styles.statsRow}>
+                            <IconStatsCard
+                                // imageContent={
+                                //     <Image
+                                //         source={require('../../assets/icons/bin.png')}
+                                //     />
+                                // }
+                                value="324,786"
+                                title="Total Litter"
+                                contentCenter
+                                backgroundColor="#FDE5E5"
+                                fontColor="#E12F2E"
+                            />
+                            <IconStatsCard
+                                contentCenter
+                                value="181,477"
+                                title="Total Photos"
+                                backgroundColor="#FDF2D3"
+                                fontColor="#997028"
+                            />
+                        </View>
+                        <View style={styles.statsRow}>
+                            <IconStatsCard
+                                // imageContent={
+                                //     <Image
+                                //         source={require('../../assets/icons/bin.png')}
+                                //     />
+                                // }
+                                value="26,743"
+                                title="Total Littercoins"
+                                contentCenter
+                                backgroundColor="#ECEEFF"
+                                fontColor="#2C45FF"
+                            />
+                            <IconStatsCard
+                                contentCenter
+                                value="4,748"
+                                title="Total Users"
+                                backgroundColor="#DEFFF8"
+                                fontColor="#1F6E5D"
+                            />
+                        </View>
+                    </View>
+                </ScrollView>
             </>
         );
     }
@@ -28,8 +88,15 @@ class StatsScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: 'white'
+    },
+    statsContainer: {
+        marginTop: 20,
+        padding: 10
+    },
+    statsRow: {
+        justifyContent: 'space-between',
+        flexDirection: 'row'
     }
 });
 
---
+++ screens/stats/_components/IconStatsCard.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
+import { Colors, Title, Caption } from '../../components';
+const { width } = Dimensions.get('window');
+
+const IconStatsCard = ({
+    style,
+    value,
+    title,
+    fontColor,
+    imageContent,
+    contentCenter,
+    backgroundColor = Colors.accent
+}) => {
+    return (
+        <View
+            style={[
+                styles.container,
+                contentCenter && { alignItems: 'center' },
+                {
+                    backgroundColor
+                },
+                style
+            ]}>
+            {imageContent}
+            <Title
+                style={[
+                    contentCenter && { textAlign: 'center' },
+                    { color: fontColor }
+                ]}>
+                {value}
+            </Title>
+            <Caption
+                family="regular"
+                style={[
+                    contentCenter && { textAlign: 'center' },
+                    { color: fontColor }
+                ]}>
+                {title}
+            </Caption>
+        </View>
+    );
+};
+
+export default IconStatsCard;
+
+IconStatsCard.propTypes = {
+    value: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    style: PropTypes.any,
+    fontColor: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    contentCenter: PropTypes.bool,
+    imageContent: PropTypes.element
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#cbd8ff',
+        justifyContent: 'center',
+        padding: 20,
+        borderRadius: 12,
+        margin: 10,
+        width: width / 2 - 30
+        // flex: 1
+    }
+});
