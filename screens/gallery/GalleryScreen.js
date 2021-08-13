e54577ac66b82388e9a62a3fef307bdb8a033b32
deznit <deznit.official@gmail.com>
2021-08-13 19:29:56 +0530

    feat: gallery image flatlist view

--- screens/gallery/GalleryScreen.js
+++ screens/gallery/GalleryScreen.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, Pressable } from 'react-native';
+import {
+    Text,
+    StyleSheet,
+    View,
+    Pressable,
+    FlatList,
+    Dimensions,
+    Image
+} from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as actions from '../../actions';
 import { Header, SubTitle, Body } from '../components';
 
+const { width } = Dimensions.get('window');
 class GalleryScreen extends Component {
     constructor(props) {
         super(props);
     }
+
+    renderImage({ item, index }) {
+        // console.log(JSON.stringify(item.item, null, 2));
+        return (
+            <Image
+                key={item.uri}
+                source={{ uri: item.uri }}
+                style={{
+                    width: width / 3 - 2,
+                    height: width / 3 - 2,
+                    margin: 1
+                }}
+            />
+        );
+    }
     render() {
+        const { geotaggedImages } = this.props;
+        console.log(JSON.stringify(geotaggedImages, null, 2));
         return (
             <>
                 <Header
@@ -37,6 +63,16 @@ class GalleryScreen extends Component {
                         </Pressable>
                     }
                 />
+                <View style={{ flexDirection: 'row' }}>
+                    <FlatList
+                        style={{ flexDirection: 'column' }}
+                        numColumns={3}
+                        data={geotaggedImages}
+                        renderItem={(item, index) =>
+                            this.renderImage(item, index)
+                        }
+                    />
+                </View>
             </>
         );
     }
